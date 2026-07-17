13646be3a3dbcf8dae99cb0aceea53c88177d4af
zarik <zarik2007t@gmail.com>
2026-07-17 21:42:36 +0200

    Add Bean for TestContainer to check  if everything works. Add local database implemented by docker-compose. Remove OAuth2, it'll be added later.

--- src/test/java/collabdesk/TestCollabDeskApplication.java
+++ src/test/java/collabdesk/TestCollabDeskApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 public class TestCollabDeskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(CollabDeskApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.
+                from(CollabDeskApplication::main).
+                with(TestcontainersConfiguration.class).
+                run(args);
     }
 
 }
--- src/test/java/collabdesk/TestcontainersConfiguration.java
+++ src/test/java/collabdesk/TestcontainersConfiguration.java
@@ -1,8 +1,15 @@
 package collabdesk;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.mysql.MySQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
+    @Bean
+    @ServiceConnection
+    public MySQLContainer mysqlContainer() {
+        return new MySQLContainer("mysql:8.4.10");
+    }
 }

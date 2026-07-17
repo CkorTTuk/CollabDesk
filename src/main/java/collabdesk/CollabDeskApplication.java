0ae286a1af8fdb888b2bd7253f713025cc0f0240
zarik <zarik2007t@gmail.com>
2026-07-17 14:36:14 +0200

    First commit, goal is to create a service where users create workspaces, invite participants, create projects and tasks, assign responsibility, and leave comments.

---
+++ src/main/java/collabdesk/CollabDeskApplication.java
@@ -0,0 +1,13 @@
+package collabdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/collabdesk/TestCollabDeskApplication.java
@@ -0,0 +1,11 @@
+package collabdesk;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCollabDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CollabDeskApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
